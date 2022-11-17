7b9e62756da81242e5e2b100774d2dedf7432c4e
mikechirkov44 <ya.mikechirkov@yandex.ru>
2022-11-17 14:32:36 +0300

    backend settings

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -1,9 +1,36 @@
-from django.contrib import admin
-
-from .models import Customer
+from django.utils.translation import ngettext
+from django.contrib import admin, messages
+from .models import Customer, Material
 
 
 @admin.register(Customer)
-class UserAdmin(admin.ModelAdmin):
+class CustomerAdmin(admin.ModelAdmin):
+    actions = ['move_to_archive']
     list_display = ('pk', 'customer_name', 'phone_number',
                     'request_type', 'email', 'is_contacted',)
+    list_filter = ('is_contacted',)
+    search_fields = ('email', 'customer_name',)
+    list_per_page = 10
+
+    @admin.action(description='Пометить контакт как состоявшимся')
+    def move_to_archive(self, request, queryset):
+        queryset.update(is_contacted=True)
+
+        updated = queryset.update(is_contacted=True)
+        self.message_user(request, ngettext(
+            '%d контакт помечен как состоявшийся!',
+            '%d контакт помечены как состоявшиеся!',
+            updated,
+        ) % updated, messages.SUCCESS)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description', 'for_clients')
+    list_filter = ('for_clients',)
+    search_fields = ('title',)
+    list_per_page = 10
+
+
+admin.site.site_title = 'DK-CONSULTING ADMIN'
+admin.site.site_header = 'DK-CONSULTING ADMIN'
--- backend/mainapp/filters.py
+++ backend/mainapp/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
 
-from .models import Customer
+from .models import Customer, Material
 
 
 class CustomerModelFilter(filters.FilterSet):
@@ -8,3 +8,10 @@ class CustomerModelFilter(filters.FilterSet):
     class Meta:
         model = Customer
         fields = ["request_type", "is_contacted"]
+
+
+class MaterialModelFilter(filters.FilterSet):
+
+    class Meta:
+        model = Material
+        fields = ["for_clients", ]
